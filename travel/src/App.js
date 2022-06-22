c7458014a6f5d522c5cb5ca70be338d2678ca72d
MLDODSON <thinkwellpub@gmail.com>
2022-06-22 10:14:03 -0500

    Branch - 1
    Branch - 1. Project starter files and setup.

---
+++ travel/src/App.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import Loading from "./comps/Loading";
+import Tours from "./comps/Tours";
+const url = "https://app.jsonstorage.net/items/8415dd72-cc43-4cca-b514-6d09c630ba87"
+
+const App = () => {
+  return (
+    <div>
+      <h1>Dominican Tours</h1>
+    </div>
+  )
+}
+
+export default App;
